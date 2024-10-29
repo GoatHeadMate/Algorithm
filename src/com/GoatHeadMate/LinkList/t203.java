7c9eadaa9912a2d9a679af732e53b21a7c65989f
GoatHead Mate <goatheadmate@163.com>
2024-10-29 16:46:53 +0800

    t209

--- src/com/GoatHeadMate/LinkList/t203.java
+++ src/com/GoatHeadMate/LinkList/t203.java
@@ -2,34 +2,35 @@ package com.GoatHeadMate.LinkList;
 
 /**
  * Author: GoatHead Mate
- * DATA: 2024/9/18-09-18-下午5:40
+ * DATA: 2024/10/29-10-29-下午4:35
  * Description: com.GoatHeadMate.LinkList
  * Version: 1.0
  */
 
+
 class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
 public class t203 {
-      public ListNode removeElements(ListNode head, int val) {
-            if(head == null) {
-                  return null;
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) {
+            return null;
+        }
+        ListNode listNode=new ListNode(0);
+        listNode.next=head;
+        ListNode current=listNode;
+        while(current.next!=null){
+            if(current.next.val == val){
+                current.next=current.next.next;
+            }else {
+                current=current.next;
             }
-            ListNode listNode=new ListNode(0);
-            listNode.next=head;
-            ListNode current=listNode;
-            while(current.next!=null){
-                  if(current.next.val == val){
-                        current.next=current.next.next;
-                  }else {
-                        current=current.next;
-                  }
-            }
-            return listNode.next;
-      }
+        }
+        return listNode.next;
+    }
 }
